406d35e84b55bbf6e2524409d0a072f7a91b5f9e
Grom PE <i@grompe.org.ru>
2016-09-24 09:31:33 +0700

    Add tools for changelog management

---
+++ addch.py
@@ -0,0 +1,26 @@
+import re,subprocess
+
+gitcommand = 'git log origin..master --format="- %s"'
+
+news = subprocess.check_output(gitcommand)
+if len(news)<2:
+  print("Nothing new.")
+  exit()
+
+f = open("CHANGELOG.txt", "r")
+changelog = f.read()
+f.close()
+
+ver = re.match("(\d+)\.(\d+)\.(\d+)\.(\d+)", changelog)
+l = list(ver.group(1, 2, 3, 4))
+l[1] = int(l[1], 10) + 1
+newver = "%s.%s.%s.%s" % tuple(l)
+
+additions = newver + "\n" + news + "\n"
+
+f = open("CHANGELOG.txt", "w")
+f.write(additions)
+f.write(changelog)
+f.close()
+
+print(additions)
---
+++ copypost.py
@@ -0,0 +1,15 @@
+import re,subprocess
+
+template = """####[Script updated to %s](https://github.com/grompe/Drawception-ANBT#drawception-anbt-):
+
+%s
+
+"""
+
+data = open("CHANGELOG.txt").read()
+s = re.match("([\d\.]+?)\n(.+?)\n\n\d", data, re.DOTALL)
+
+post = template % s.group(1, 2)
+
+p = subprocess.Popen("clip", stdin=subprocess.PIPE)
+p.stdin.write(post)
