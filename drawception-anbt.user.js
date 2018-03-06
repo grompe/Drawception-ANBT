cad2c2fd4881ae1176e8f04563f50679cd62f7cb
MEDavy <maximiliandavy@gmail.com>
2018-03-05 18:08:32 -0600

    Fix

--- drawception-anbt.user.js
+++ drawception-anbt.user.js
@@ -249,52 +249,6 @@ function sendPost(url, params, onloadfunc, onerrorfunc, ontimeoutfunc)
 }
 
 
-//get average color from image
-function getAverageRGB(imgEl) {
-
-  var blockSize = 5, // only visit every 5 pixels
-      defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
-      canvas = document.createElement('canvas'),
-      context = canvas.getContext && canvas.getContext('2d'),
-      data, width, height,
-      i = -4,
-      length,
-      rgb = {r:0,g:0,b:0},
-      count = 0;
-
-  if (!context) {
-      return defaultRGB;
-  }
-
-  height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-  width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
-
-  context.drawImage(imgEl, 0, 0);
-
-  try {
-      data = context.getImageData(0, 0, width, height);
-  } catch(e) {
-      /* security error, img on diff domain */
-      return defaultRGB;
-  }
-
-  length = data.data.length;
-
-  while ( (i += blockSize * 4) < length ) {
-      ++count;
-      rgb.r += data.data[i];
-      rgb.g += data.data[i+1];
-      rgb.b += data.data[i+2];
-  }
-
-  // ~~ used to floor values
-  rgb.r = ~~(rgb.r/count);
-  rgb.g = ~~(rgb.g/count);
-  rgb.b = ~~(rgb.b/count);
-
-  return rgb;
-
-}
 
 function extractInfoFromHTML(html)
 {
@@ -3954,6 +3908,49 @@ function pageEnhancements()
 
   //set navbar color based on user profile
   try{
+    //get average color from image
+    function getAverageRGB(imgEl) {
+
+      var blockSize = 5, // only visit every 5 pixels
+          defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+          canvas = document.createElement('canvas'),
+          context = canvas.getContext && canvas.getContext('2d'),
+          data, width, height,
+          i = -4,
+          length,
+          rgb = {r:0,g:0,b:0},
+          count = 0;
+
+      if (!context) {
+          return defaultRGB;
+      }
+
+      height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+      width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+
+      context.drawImage(imgEl, 0, 0);
+
+      try {
+          data = context.getImageData(0, 0, width, height);
+      } catch(e) {
+          /* security error, img on diff domain */
+          return defaultRGB;
+      }
+
+      length = data.data.length;
+
+      while ( (i += blockSize * 4) < length ) {
+          ++count;
+          rgb.r += data.data[i];
+          rgb.g += data.data[i+1];
+          rgb.b += data.data[i+2];
+      }
+      // ~~ used to floor values
+      rgb.r = ~~(rgb.r/count);
+      rgb.g = ~~(rgb.g/count);
+      rgb.b = ~~(rgb.b/count);
+      return rgb;
+    }
     var navbarColor = getAverageRGB(document.getElementsByClassName("profile-avatar")[0]);
     document.getElementById("nav-drag").style.backgroundColor = navbarColor;
   }catch{
