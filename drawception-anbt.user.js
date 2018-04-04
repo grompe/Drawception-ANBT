d83102b311fae42bf5e7ce0a09f1482f289707b0
Grom PE <i@grompe.org.ru>
2018-04-04 21:39:33 +0700

    1.123.2018.04

--- drawception-anbt.user.js
+++ drawception-anbt.user.js
@@ -2,7 +2,7 @@
 // @name         Drawception ANBT
 // @author       Grom PE
 // @namespace    http://grompe.org.ru/
-// @version      1.122.2018.03
+// @version      1.123.2018.04
 // @description  Enhancement script for Drawception.com - Artists Need Better Tools
 // @downloadURL  https://raw.github.com/grompe/Drawception-ANBT/master/drawception-anbt.user.js
 // @match        http://drawception.com/*
@@ -14,7 +14,7 @@
 
 function wrapped() {
 
-var SCRIPT_VERSION = "1.122.2018.03";
+var SCRIPT_VERSION = "1.123.2018.04";
 var NEWCANVAS_VERSION = 37; // Increase to update the cached canvas
 var SITE_VERSION = "2.9.59"; // Last seen site version
 
